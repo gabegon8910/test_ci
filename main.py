2f74f858459329430d7a69c4c02196b9ad3f2586
gabegon8910 <gabe@gonzalezs.net>
2021-08-28 15:22:54 -0400

    test

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+class Calculator:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self):
+        self.x
+        self.y
+        self.total
 
+    def sum(self, x, y):
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        return self.x + self.y
 
+    def subtract(self, x, y):
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        return self.x - self.y
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def divide(self, x, y):
+        return self.y / self.y
+
+    def multiply(self, x, y):
+        return self.x * self.y
+
+    
---
+++ text.py
@@ -0,0 +1,21 @@
+from main import Calculator
+
+def test_add():
+    x,y = 1,2
+    instance = Calculator(x,y)
+    assert instance.sum() == x + y , "Add method doesn't work"
+
+def test_subtract():
+    x,y = 1,2
+    instance = Calculator(x,y)
+    assert instance.subtract() == x-y, "Subtract method doesn't work"
+
+def test_divide():
+    x,y = 1,2
+    instance = Calculator(x,y)
+    assert instance.divide() == x/y, "Divide method doesn't work"
+
+def test_multiply():
+    x,y = 1,2
+    instance = Calculator(x,y)
+    assert instance.multiply() == x*y, "Multiply method does not work"
