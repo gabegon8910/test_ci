92bff0c1412d96d85ecd9c00c53d5524a42f8410
gabegon8910 <gabe@gonzalezs.net>
2021-08-28 15:28:38 -0400

    test

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 class Calculator:
 
-    def __init__(self):
+    def __init__(self, x, y):
         self.x
         self.y
-        self.total
 
-    def sum(self, x, y):
+
+    def sum(self):
 
         return self.x + self.y
 
-    def subtract(self, x, y):
+    def subtract(self):
 
         return self.x - self.y
 
-    def divide(self, x, y):
+    def divide(self):
         return self.y / self.y
 
-    def multiply(self, x, y):
+    def multiply(self):
         return self.x * self.y
 
-    
+
+if __name__ == '__main__':
+    instance = Calculator(1,2)
+    print(instance.add())
